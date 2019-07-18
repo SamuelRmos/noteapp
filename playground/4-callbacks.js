514a163fcb416d8d92ec9eee0742cc6755136bf6
root <root@localhost.localdomain>
2019-07-17 21:26:36 -0300

    Example geocode

--- playground/4-callbacks.js
+++ playground/4-callbacks.js
@@ -1,14 +1,24 @@
 
-const geocode = (address, callback) => {
-    setTimeout(() => {
-        const data = {
-            lat: 0,
-            long:0
-        }
-        callback(data)
-    }, 2000)
+// const geocode = (address, callback) => {
+//     setTimeout(() => {
+//         const data = {
+//             lat: 0,
+//             long:0
+//         }
+//         callback(data)
+//     }, 2000)
+// }
+
+// geocode('Philadelphia', (data) => {
+//     console.log(data)
+// })
+
+const add = (a,b,callback) => {
+   setTimeout(() => {
+       callback(a + b)
+   }, 2000)
 }
 
-geocode('Philadelphia', (data) => {
-    console.log(data)
+add(1, 4, (sum) => {
+    console.log(sum)
 })
